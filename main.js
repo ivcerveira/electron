30ccb4faf1a429e5526ffe935c317700e3faee2f
Ivaldo Lima Cerveira <ivaldo.lima@petrobras.com.br>
2024-08-22 12:58:31 -0300

    Modified files

---
+++ main.js
@@ -0,0 +1,15 @@
+//console.log("Processo principal")
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('./src/views/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
